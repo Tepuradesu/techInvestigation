15a9a8d3a4d3a1016155b85e83f912806bfcab94
Tepura <tepuracococo@gmail.com>
2020-11-18 05:08:33 +0900

    second commit node.js tutorial added a new routing (top) [issu1]

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 handle["/"] = requestHandlers.start;
+handle["/top"] = requestHandlers.top;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+
 
 //ルータモジュールとhandleオブジェクトを引数に渡す。
 server.start(router.route,handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -10,12 +10,20 @@ function start(response) {
   });
 }
 
+function top(response) {
+  console.log("Request handler 'start' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello NodeJs");
+    response.end();
+}
+
 function upload(response) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello Upload");
   response.end();
 }
+
 
 exports.start = start;
 exports.upload = upload;
